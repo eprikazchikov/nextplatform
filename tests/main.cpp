a80b54105808a5a55b9d64ff7aa540b3a5e54065
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2015-01-16 09:41:27 +0300

    Multiple tests suite reporting system support implemented

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,19 +4,54 @@
 #include "tst_object.h"
 #include "tst_objectsystem.h"
 
+#define addReport(args, tpl, ts) { \
+    if(!tpl.isEmpty()) { \
+        QString arg = tpl; \
+        arg.replace('*', ts.metaObject()->className()); \
+        args.append("-o"); \
+        args.append(arg); \
+    } \
+} \
+
+#define Cleanup(args, tpl) { \
+    if(!tpl.isEmpty()) { \
+        args.removeLast(); \
+        args.removeLast(); \
+    } \
+} \
+
 int main(int argc, char *argv[]) {
+    QStringList args;
+    for(int i = 0; i < argc; i++) {
+        args.append(argv[i]);
+    }
+
+    QString tpl;
+    int index   = args.indexOf("-t");
+    if(index != -1) {
+        tpl = args.at(index + 1);
+        args.removeAt(index);
+        args.removeAt(index);
+    }
+
     int status = 0;
     {
-        VariantTest tc;
-        status |= QTest::qExec(&tc, argc, argv);
+        VariantTest ts;
+        addReport(args, tpl, ts);
+        status |= QTest::qExec(&ts, args);
+        Cleanup(args, tpl);
     }
     {
-        ObjectTest tc;
-        //status |= QTest::qExec(&tc, argc, argv);
+        ObjectTest ts;
+        addReport(args, tpl, ts);
+        status |= QTest::qExec(&ts, args);
+        Cleanup(args, tpl);
     }
     {
-        ObjectSystemTest tc;
-        //status |= QTest::qExec(&tc, argc, argv);
+        ObjectSystemTest ts;
+        addReport(args, tpl, ts);
+        status |= QTest::qExec(&ts, args);
+        Cleanup(args, tpl);
     }
     return status;
 }
