abb94d58c33feba85cb41fd026d1fdda98590254
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2018-03-21 21:18:56 +0300

    Tests improvements

--- tests/tst_animation.cpp
+++ tests/tst_animation.cpp
@@ -15,40 +15,47 @@ void AnimationTest::Variant_animation() {
     VariantAnimation anim;
     anim.setLoopDuration(1000);
     {
-        anim.setKeyFrames({ KeyFrame(0.0f, 0), KeyFrame(1.0f, 10) });
+        anim.setKeyFrames({ KeyFrame(0.0f, 0.0f),
+                            KeyFrame(1.0f, 10.0f) });
         anim.setCurrentTime(200);
         QCOMPARE(anim.currentValue().toInt(), 2);
     }
     {
-        anim.setKeyFrames({ KeyFrame(0.0f, 1.0f), KeyFrame(1.0f, 2.0f) });
+        anim.setKeyFrames({ KeyFrame(0.0f, 1.0f),
+                            KeyFrame(1.0f, 2.0f) });
         anim.setCurrentTime(250);
         QCOMPARE(anim.currentValue().toFloat(), 1.25f);
     }
     {
-        anim.setKeyFrames({ KeyFrame(0.0f, Vector2(1, 2)), KeyFrame(1.0f, Vector2(3, 4)) });
+        anim.setKeyFrames({ KeyFrame(0.0f, Vector2(1.0f, 2.0f)),
+                            KeyFrame(1.0f, Vector2(3.0f, 4.0f)) });
         anim.setCurrentTime(300);
         QCOMPARE(anim.currentValue().toVector2(), Vector2(1.6f, 2.6f));
     }
     {
-        anim.setKeyFrames({ KeyFrame(0.0f, Vector3(1, 2, 3)), KeyFrame(1.0f, Vector3(3, 4, 5)) });
+        anim.setKeyFrames({ KeyFrame(0.0f, Vector3(1.0f, 2.0f, 3.0f)),
+                            KeyFrame(1.0f, Vector3(3.0f, 4.0f, 5.0f)) });
         anim.setCurrentTime(300);
         QCOMPARE(anim.currentValue().toVector3(), Vector3(1.6f, 2.6f, 3.6f));
     }
     {
-        anim.setKeyFrames({ KeyFrame(0.0f, Vector4(1, 2, 3, 4)), KeyFrame(0.5f, Vector4(4, 5, 6, 7)), KeyFrame(1.0f, Vector4(3, 4, 5, 6)) });
+        anim.setKeyFrames({ KeyFrame(0.0f, Vector4(1.0f, 2.0f, 3.0f, 4.0f)),
+                            KeyFrame(0.5f, Vector4(4.0f, 5.0f, 6.0f, 7.0f)),
+                            KeyFrame(1.0f, Vector4(3.0f, 4.0f, 5.0f, 6.0f)) });
         anim.setCurrentTime(500);
         Vector4 v   = anim.currentValue().toVector4();
-        QCOMPARE(v, Vector4(4, 5, 6, 7));
+        QCOMPARE(v, Vector4(4.0f, 5.0f, 6.0f, 7.0f));
 
         anim.setCurrentTime(750);
-        QCOMPARE(anim.currentValue().toVector4(), Vector4(3.5, 4.5, 5.5, 6.5));
+        QCOMPARE(anim.currentValue().toVector4(), Vector4(3.5f, 4.5f, 5.5f, 6.5f));
     }
     {
-        anim.setKeyFrames({ KeyFrame(0.0f, Quaternion(Vector3(0, 1, 0), 0)), KeyFrame(1.0f, Quaternion(Vector3(0, 1, 0), 90)) });
+        anim.setKeyFrames({ KeyFrame(0.0f, Quaternion(Vector3(0.0f, 1.0f, 0.0f), 0.0f)),
+                            KeyFrame(1.0f, Quaternion(Vector3(0.0f, 1.0f, 0.0f), 90.0f)) });
         anim.setCurrentTime(500);
 
         Quaternion result   = anim.currentValue().toQuaternion();
-        Quaternion expected = Quaternion(Vector3(0, 1, 0), 45);
+        Quaternion expected = Quaternion(Vector3(0.0f, 1.0f, 0.0f), 45.0f);
         //QCOMPARE(result, expected);
     }
 }
@@ -61,8 +68,9 @@ void AnimationTest::Property_animation() {
     anim.setTarget(&object, "vec");
     QCOMPARE((anim.target() != nullptr), true);
 
-    anim.setKeyFrames({ KeyFrame(0.0f, Vector2(0, 0)), KeyFrame(1.0f, Vector2(1, 2)) });
+    anim.setKeyFrames({ KeyFrame(0.0f, Vector2(0.0f, 0.0f)),
+                        KeyFrame(1.0f, Vector2(1.0f, 2.0f)) });
     anim.setCurrentTime(500);
 
-    QCOMPARE(object.getVector(), Vector2(0.5, 1));
+    QCOMPARE(object.getVector(), Vector2(0.5, 1.0f));
 }
--- tests/tst_math.cpp
+++ tests/tst_math.cpp
@@ -6,6 +6,12 @@
 #include <QQuaternion>
 
 void MathTest::Euler_angles() {
+    QVector3D angles(90.0f, 90.0f, 90.0f);
+    QQuaternion q   = QQuaternion::fromEulerAngles(angles);
+    QVector3D euler = q.toEulerAngles();
+
+    qDebug() << angles << euler;
+
     {
         Matrix3 m;
         m.rotate(Vector3(90.0f));
